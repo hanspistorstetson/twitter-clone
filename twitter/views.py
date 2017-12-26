8fdc94e1d4bbd518be8e8489564e12ddfe46ac4a
Hans Pistor <hpistor@stetson.edu>
2017-12-26 16:06:19 -0500

    Refactored index view to use template

--- twitter/views.py
+++ twitter/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def index(self):
-    return HttpResponse('<html><title>Twitter</title></html>')
+def index(request):
+    return render(request, 'twitter/index.html')
